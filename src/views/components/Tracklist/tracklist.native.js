e2055ce667ed3a1c02e0be1d5830df02139ede9f
Kia Rahimian <kia@tintmail.com>
2018-07-05 08:17:05 -0400

    feat: add icons

--- src/views/components/Tracklist/tracklist.native.js
+++ src/views/components/Tracklist/tracklist.native.js
@@ -4,17 +4,34 @@ import { createSelector } from 'reselect'
 import { View } from 'react-native'
 
 import { getCurrentTracklist, getTracksForCurrentTracklist } from '@core/tracklists'
+import { getPlayerIsPlaying, getPlayerTrackId, playerActions } from '@core/player'
+import { audio } from '@core/audio'
 import LoadingIndicator from '@components/LoadingIndicator'
 import Track from '@components/Track'
 
 class Tracklist extends React.Component {
   render () {
-    const { tracks } = this.props
+    const {
+      isPlaying,
+      pause,
+      play,
+      selectedTrackId,
+      selectTrack,
+      tracklistId,
+      tracks
+    } = this.props
 
     const trackItems = tracks.map((track, index) => {
+      const isSelected = track.id === selectedTrackId
       return (
         <View key={index}>
-          <Track track={track} />
+          <Track
+            track={track}
+            isPlaying={isSelected && isPlaying}
+            isSelected={isSelected}
+            pause={pause}
+            play={isSelected ? play : selectTrack.bind(null, track.id, tracklistId)}
+          />
         </View>
       )
     })
@@ -32,12 +49,26 @@ class Tracklist extends React.Component {
 }
 
 const mapStateToProps = createSelector(
+  getPlayerIsPlaying,
+  getPlayerTrackId,
   getCurrentTracklist,
   getTracksForCurrentTracklist,
-  (tracklist, tracks) => ({
+  (isPlaying, playerTrackId, tracklist, tracks) => ({
     displayLoadingIndicator: tracklist.isPending,
+    isPlaying,
+    pause: audio.pause,
+    play: audio.play,
+    selectedTrackId: playerTrackId,
+    tracklistId: tracklist.id,
     tracks
   })
 )
 
-export default connect(mapStateToProps, null)(Tracklist)
+const mapDispatchToProps = {
+  selectTrack: playerActions.playSelectedTrack
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Tracklist)
